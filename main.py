0f47242cb347b2515dda3de0ea8aca07b64c42ff
Maria Bucura <ronnie_ssj4@yahoo.com>
2021-10-20 11:57:24 +0300

    Prima cerinta

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-def print_integers(lst)
-    
+
+
+
 
 
 def main():
     while True:
         print('1. Citire date. ')
-        print('2.')
+        print('2. Afisarea numerelor intregi din lista')
         print('3.')
         print('4.')
         print('x. Iesire.')
         optiune = input('Alege optiunea: ')
         if optiune == '1':
-            lst = []
-            n = int(input('Dimensiunea listei: '))
+            A = []
+            B = []
+            n = int(input('dimensiune prima multime: '))
+            m = int(input('dimensiune a doua multime: '))
             for i in range(0, n):
-                e = float(input())
-                lst.append(e)
-            print(lst)
-        elif potiune == 'x':
+                e = int(input())
+                A.append(e)
+            for i in range(0, m):
+                e = int(input())
+                B.append(e)
+            print(A,B)
+
+        elif optiune == 'x':
             break
         else:
             print('Optiune Invalida!')
 
 main()
+
